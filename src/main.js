ae8207fcd3ba8ba550b49bed59fdd4db0a2312cc
SimoLomba53 <simolomba253@gmail.com>
2023-03-06 17:19:44 +0100

    installazione country flag

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
+import "bootstrap/dist/css/bootstrap.css";
 
-import 'bootstrap/dist/css/bootstrap.css'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import CountryFlag from "vue-country-flag";
+Vue.component("country-flag", CountryFlag);
 
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+createApp(App).mount("#app");
 
-
-
-
-createApp(App).mount('#app')
-
-import  'bootstrap/dist/js/bootstrap.bundle.js'
+import "bootstrap/dist/js/bootstrap.bundle.js";
