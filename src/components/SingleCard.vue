9e419c39daba3900f20a636f4b64bdeaac12e5eb
SimoLomba53 <simolomba253@gmail.com>
2023-03-06 19:02:17 +0100

    flag non visibili

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -1,24 +1,37 @@
 <script>
-   export default {
-    props: {
-        title:String,
-        originalname:String,
-        language:String,
-        vote:String,
+export default {
+  props: {
+    title: String,
+    originalname: String,
+    language: String,
+    vote: String,
+  },
+
+  methods: {
+    getFlag(language) {
+      if (language == "it") return "https://url/to/ita/flag";
+      if (language == "en") return "https://url/to/eng/flag";
+      if (language == "jp") return "https://url/to/jpn/flag";
+      if (language == "es") return "https://url/to/esp/flag";
+      if (language == "ru") return "https://url/to/rus/flag";
+
+      return `https://url/to/${language}/flag`;
     },
- 
-    
+  },
 };
 </script>
 
 <template>
-    <div class="text-center text-light">
+  <div class="text-center text-light">
     <h1>{{ title }}</h1>
     <p>{{ originalname }}</p>
-    <p>{{ language }}</p>
+    <img id="flag" :src="getFlag(language)" alt="" />
     <p>{{ vote }}</p>
-    </div>
-
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#flag {
+  width: 10px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import "bootstrap/dist/css/bootstrap.css";
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import CountryFlag from "vue-country-flag";
-Vue.component("country-flag", CountryFlag);
 
 createApp(App).mount("#app");
 
